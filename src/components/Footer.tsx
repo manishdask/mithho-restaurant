1dd53f646db5bb76c49fcb362b9b5b1d10389ee7
manishdask <manishkd120@gmail.com>
2026-02-16 14:54:36 +1100

    Update address format in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -99,7 +99,7 @@ function Footer() {
             <ul className="space-y-4">
               <li className="flex items-start space-x-3">
                 <MapPin className="w-5 h-5 text-amber-400 flex-shrink-0 mt-1" />
-                <span className="text-gray-400">2A Vaughan St, Lidcombe NSW 2141</span>
+                <span className="text-gray-400">shop 9, 2A Vaughan St, Lidcombe NSW 2141</span>
               </li>
               <li className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-amber-400 flex-shrink-0" />
